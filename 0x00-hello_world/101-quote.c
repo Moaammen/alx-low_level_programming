0b96efc83271f526b67e35c4014a9311095350ac
Moaammen <moaammmeen@gmail.com>
2022-12-09 04:32:04 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main (void)
 {
-  printf ("and that piece of art is useful"" - Dora Korpar, 2015-10-19");
+  printf ("and that piece of art is useful""\""" - Dora Korpar, 2015-10-19\n");
     return (0);
 
 
