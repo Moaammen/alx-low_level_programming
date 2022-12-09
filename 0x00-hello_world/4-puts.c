8346910ee6481d3803d5bfe046f14abcd70da989
Moaammen <moaammmeen@gmail.com>
2022-12-08 18:18:12 -0800

    betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
+
+
 /**
- *main -entry point
- *return always 0 successfuly
+
+ *main - Entry point
+
+ *Return: Always 0 (Success)
+
  */
+
+
+
 int main(void)
+  
 {
- 
-  puts("\"Programming is like building a multilingual puzzle");  
+  
+  puts("\"Programming is like building a multilingual puzzle");
+  
   return (0);
   
 }
