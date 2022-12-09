62c6831990062335418c45d31dcd2bfcb63c3878
Moaammen <moaammmeen@gmail.com>
2022-12-09 04:24:06 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ *
+ *Return: Always 0 (Success)
+ */
+int main (void)
+{
+  printf ("and that piece of art is useful"" - Dora Korpar, 2015-10-19");
+    return (0);
+
+
+
+}
