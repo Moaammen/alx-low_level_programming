28faeb051198eb7fa3987b37572e71e945b2182c
Moaammen <moaammmeen@gmail.com>
2022-12-09 04:40:18 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,11 @@
  *
  *Description: C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
  *
- *Return: Always 0 (Success)
+ *Return: Always 1 (Success)
  */
 int main (void)
+ 
 {
-  printf ("and that piece of art is useful""\""" - " "Dora Korpar"", " "2015-10-19\n");
-    return (0);
-
-
-
+write ("and that piece of art is useful""\""" - " "Dora Korpar"", " "2015-10-19\n");
+ return (1);
 }
