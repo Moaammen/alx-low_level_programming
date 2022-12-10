8a9370a328fba0ab4e8be81e497f9d9a3c43c3df
Moaammen <moaammmeen@gmail.com>
2022-12-09 17:54:16 -0800

    more alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,17 +12,17 @@ int main (void)
 {
 char i;
 
-for (i > 'a'; i <= 'z' ; i++)
+for (i = 'a'; i <= 'z' ; i++)
 
 {
 
-if(i == e)
-{
+if(i == 'e')
+
 continue;
-}
-else if(i == q)
+
+else if(i == 'q')
 continue;
-}
+
 putchar(i);
 
 }
