2c024b14ef14c3dc166b2268494ed190ed192446
Moaammen <moaammmeen@gmail.com>
2022-12-09 18:24:56 -0800

    more numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Descriotion: print all single numbers using while loop
+ *
+ *Return: 0 Success
+ *
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
