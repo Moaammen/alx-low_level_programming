6eba1edafc43656383949756caa397a99aef64ba
Moaammen <moaammmeen@gmail.com>
2022-12-09 18:29:09 -0800

    more alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: use putchar function to print alphabet
+ *
+ *Return: 0 Success
+ *
+ */
+
+int main(void)
+{
+
+char i;
+
+for (i <= 'z' ; i = 'a' ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
