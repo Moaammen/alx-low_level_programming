b138d312a388bccfaecbe8705bd000fb687cae3d
Moaammen <moaammmeen@gmail.com>
2022-12-09 18:08:21 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Descriotion: print all single numbers using while loop
+ *
+ *Return: 0 Success
+ *
+ */
+int main(void)
+{
+int i;
+
+while (i<10)
+{
+printf ("%i", i);
+i++;
+}
+printf ("\n");
+return (0);
+}
