ebfc0d48b1ed9015861a74d5887314562bf76127
Moaammen <moaammmeen@gmail.com>
2022-12-09 16:40:53 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: use putchar function to print alphabet
+ *
+ *Return: 0 Success
+ *
+ */
+
+int main <void>
+{
+
+char i;
+
+for(i = 'a' ; i <= 'z' ; i++)
+{
+putchar(i);
+}
+return(0);
+}
