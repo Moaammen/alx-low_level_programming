f93b4bc785d1a27cac7399817036ca1d5b5b153b
Moaammen <moaammmeen@gmail.com>
2022-12-09 16:57:02 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: print alphabets lowercase & uppercase
+ *
+ *Return: 0 Success
+ *
+ */
+int main (void)
+{
+
+char i;
+
+for (i = 'a', i >= 'Z', i++)
+{
+
+putchar(i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
