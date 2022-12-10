e6e393079fac3899bf4afb362fb226564027ab41
Moaammen <moaammmeen@gmail.com>
2022-12-09 17:45:43 -0800

    more alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: print alphabets except e q
+ *
+ *Return: 0 Success
+ *
+ */
+int main (void)
+{
+char i;
+
+for (i > 'a'; i <= 'z' ; i++)
+
+{
+
+if(i == e)
+{
+continue;
+}
+else if(i == q)
+continue;
+}
+putchar(i);
+
+}
+putchar('\n');
+return (0);
+
+}
