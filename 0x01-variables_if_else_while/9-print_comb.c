173bd404ab38c6e60caf608122fefbe7143ddfa5
Moaammen <moaammmeen@gmail.com>
2022-12-11 11:27:51 -0800

    ASCII

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Descriotion: print all single number and , 
+ *
+ *Return: 0 Success
+ *
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar (i);
+
+if (i < 57)
+{
+
+putchar (44);
+
+}
+
+
+if (i < 57)
+
+{
+
+putchar (32);
+
+}
+
+}
+
+putchar ('\n');
+
+return (0);
+
+}
