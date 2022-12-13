c1db422ae50ad32010c0d25f6bfed534c4426896
Moaammen <moaammmeen@gmail.com>
2022-12-13 12:51:58 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,17 @@
 #include "main.h"
 
+
+/**
+ *main - entry point
+ *
+ *Description: print alphabet
+ *
+ *Return: 0 Success
+ */
+
 void print_alphabet(void)
 {
-char a;
+char a = 'a';
 while (a <= 'z')
 {
 _putchar (a);
