f26d55cb749fc3ebb6e441602d927a45b2d18382
Moaammen <moaammmeen@gmail.com>
2022-12-13 13:05:19 -0800

    more alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - function
+ *
+ *Description: print_alphabet_x10
+ *
+ *Return: 0 Success
+ */
+
+void print_alphabet(void)
+{
+char a = 'a';
+char b = '0';
+while (b <= '9')
+{
+b++;
+while (a <= 'z')
+{
+_putchar (a);
+a++;
+
+}
+_putchar ('\n');
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,7 @@ int _putchar(char c);
  *print alphabet function
  */
 void print_alphabet(void);
+/**
+ *print alphabet x10 function
+ */
+void print_alphabet_x10(void);
