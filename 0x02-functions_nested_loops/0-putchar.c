381fbab929c217d3290fa5af1cd75068a3c1a296
Moaammen <moaammmeen@gmail.com>
2022-12-13 10:38:30 -0800

    return 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Description: use putchar
+ *
+ *Return: 0 Success
+ *
+ */
+int main(void)
+{
+char i = 1;
+
+ if (i != '0')
+   {
+     return (0);
+   }
+ return (0);
+}
