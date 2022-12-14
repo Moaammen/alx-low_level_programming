2aff94c9189c6e7c29ea8a83ab3f5632f95ece12
Moaammen <moaammmeen@gmail.com>
2022-12-14 11:29:21 -0800

    fix the codde

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- *
  *print_last_digit - fucntion for the last digit
- *
+ *Description: print the last digit
  *Return: the last digit
- *
  */
 int print_last_digit(int n)
 {
@@ -13,11 +11,13 @@ int last_digit;
  if (n < 0)
 {
 last_digit = (-1 * (n % 10));
+ _putchar ('0' + last_digit);
 return (last_digit);
 }
 else
 {
 last_digit = n % 10;
+_putchar ('0' + last_digit);
 return (last_digit);
 }
 }
