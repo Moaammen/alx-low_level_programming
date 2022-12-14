788677c7957ce4b53c2cc12b0c2277e93f264259
Moaammen <moaammmeen@gmail.com>
2022-12-14 14:49:59 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ if (c <= 9)
 {
 
 _putchar ((c % 10) + '0');
-if ( b <= 9)
+if ( b < 9)
 {
 _putchar (',');
 _putchar (' ');
@@ -32,7 +32,7 @@ else
 {
 _putchar ((c / 10) + '0');
 _putchar ((c % 10) + '0');
-if (b <= 9)
+if (b < 9)
 {
 _putchar (',');
 _putchar (' ');
