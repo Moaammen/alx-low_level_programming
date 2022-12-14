7c1c2fec0b4ee22336115b2dbfa0926e2ff633ba
Moaammen <moaammmeen@gmail.com>
2022-12-14 15:54:19 -0800

    print to 98 function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,21 +4,25 @@
  *print_to_98 - function
  *
  *Description: print to 98 or for it
- *
+ *n: the input
  *Return: 0 success
  */
 
 void print_to_98(int n)
 {
-while (n <= 98)
+while (n < 98)
 {
-printf ("%d, ", n);
+printf("%d, ", n);
 n++;
-while ( n >= 98)
+while( n > 98)
 {
-printf ("%d, ", n);
+printf("%d, ", n);
 n--;
 }
+if (n == 98)
+{
+printf("%d, ", n);
 }
- printf ("\n");
+}
+printf ("\n");
 }
