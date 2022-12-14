09392095efe4226715799c7be667b5a7eb1e4756
Moaammen <moaammmeen@gmail.com>
2022-12-14 13:56:21 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,9 @@ void times_table(void)
 int a;
 int b;
 int c;
-for (a = 0; a <= 9; a++)
+for (a = '0'; a <= '9'; a++)
 {
-for (b = 0; b <= 9; b++)
+for (b = '0'; b <= '9'; b++)
 {
 c = a * b;
 _putchar (c + '0');
