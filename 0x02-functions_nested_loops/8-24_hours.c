f553d1b564e33dcc76c7e6afe5026de666f298dc
Moaammen <moaammmeen@gmail.com>
2022-12-14 12:06:19 -0800

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *jack_bauer - function
+ *
+ *Description: print 24 hours
+ *
+ *Return : 0 Success
+ */
+void jack_bauer(void);
+{
+char h1;
+char h2;
+char m1;
+char m2;
+
+for (h1 = '0'; h1 <= '2'; h1++)
+{
+for (h2 = '0'; h2 <= '3'; h2++)
+{
+for (m1 = '0'; m1 <= '5'; m1++)
+{
+for (m2 = '0'; m2 <= '9'; m2++)
+{
+_putchar (h1);
+_putchar (h2);
+_putchar (':');
+_putchar (m1);
+_putchar (m2);
+_putchar ('\n');
+}
+}
+}
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -33,3 +33,7 @@ int _abs(int n);
  *print the last digit
  */
 int print_last_digit(int n);
+/**
+ *print 24 hours in minutes
+ */
+void jack_bauer(void);
