18aaeb77e78830abd5bd6d7ce827fabd001707ed
Moaammen <moaammmeen@gmail.com>
2022-12-14 11:24:47 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,15 @@
  */
 int print_last_digit(int n)
 {
-int last_digit = n % 10;
+int last_digit;
+ if (n < 0)
+{
+last_digit = (-1 * (n % 10));
 return (last_digit);
+}
+else
+{
+last_digit = n % 10;
+return (last_digit);
+}
 }
