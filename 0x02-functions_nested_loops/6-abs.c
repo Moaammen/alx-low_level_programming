a93d828ba5f3b2cdf06108b4ae4fd56a851cde06
Moaammen <moaammmeen@gmail.com>
2022-12-14 10:47:35 -0800

    update abs code

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,20 @@
 /**
  *_abs - function return the abslout value of int
  *
- *Description: Return the abslout value
+ *Description: Return the abslout value of n
  *
  *Return: 0 Success
  *
  */
 int _abs(int n)
 {
-
+if (n < 0)
+{
 int result = n * -1;
-return result;
+return (result);
+}
+else
+{
+return (n);
+}
 }
