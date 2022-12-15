b9abc9435ddfa83777c6d9442052ab444f2a6d84
Moaammen <moaammmeen@gmail.com>
2022-12-15 15:18:33 -0800

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,16 @@
 
 void print_line(int n)
 {
-int counter = 0;
-while (counter <= n)
-{
 if (n <= 0)
 {
 _putchar ('\n');
 }
-_putchar (95);
-counter++;
+else
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+_putchar ('_');
 }
-_putchar ('\n');
+_putchar ('\n')
 }
