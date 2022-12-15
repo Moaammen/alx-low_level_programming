1c066e94a08f5c03b9a9a505cbba20ce923c0031
Moaammen <moaammmeen@gmail.com>
2022-12-15 11:40:03 -0800

    is adigit ?

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - function
+ * @c: integer
+ * Description: check for a digit
+ * Return: 1 if c a digit and 0 otherwise
+ */
+
+int _isdigit (int c)
+{
+if (c <= 9 && c >= 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,7 @@
  * _isupper: function check for uppercase char
  */
 int _isupper(int c);
+/**
+ * _isdigit: function check for digit
+ */
+int _isdigit(int c);
