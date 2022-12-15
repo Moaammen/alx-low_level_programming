97d6e671ebcbf525f65c465d15ba5932178f8971
Moaammen <moaammmeen@gmail.com>
2022-12-15 12:12:25 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - function print numbers 
+ *@c: charater
+ * Return: 0 success
+ */
+
+void print_numbers(void);
+{
+char c;
+
+while (c <= '9')
+{
+_putchar (c);
+
+c++;
+}
+_putchar ('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,8 @@
 /**
+ * _putchar: function print single char
+ */
+int _putchar(char c);
+/**
  * _isupper: function check for uppercase char
  */
 int _isupper(int c);
@@ -10,3 +14,7 @@ int _isdigit(int c);
  * mul: function multiplies two integers
  */
 int mul(int a, int b);
+/**
+ * print_numbers: function print for 0 to 9 with _putchar
+ */
+void print_numbers(void);
