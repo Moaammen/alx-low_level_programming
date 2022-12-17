1f9904b4942044f0972b6359cc8526d6257a3896
Moaammen <moaammmeen@gmail.com>
2022-12-17 15:13:55 -0800

    print time tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *print_times_table - function
  *@n: integer
@@ -9,14 +8,9 @@
 void print_times_table(int n)
 {
 if (n < 0 || n >= 15)
-{
-
-}
 else
 {
-int a;
-int b;
-int c;
+int a, b, c
 for (a = 0; a <= n; a++)
 {
 for (b = 0; b <= n; b++)
@@ -24,32 +18,30 @@ for (b = 0; b <= n; b++)
 c = a * b;
 if (b == 0)
 {
-
 _putchar ('0' + c);
 }
-else if (c <= 9)
+else
 {
 _putchar (',');
 _putchar (' ');
+if (c <= 9)
+{
 _putchar (' ');
 _putchar (' ');
 _putchar ('0' + c);
 }
 else if (c > 9 && c < 100)
 {
-_putchar (',');
-_putchar (' ');
 _putchar (' ');
 _putchar ('0' + (c / 10));
 _putchar ('0' + (c % 10));
 }
 else if (c >= 100)
 {
-_putchar (',');
-_putchar (' ');
 _putchar ('0' + (c / 100));
 _putchar ('0' + ((c / 10) % 10));
 _putchar ('0' + (c % 10));
+}
 }
 }
 _putchar ('\n');
