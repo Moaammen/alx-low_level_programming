57186502cb7319cdc7cd4a0c0f19050611a82a6c
Moaammen <moaammmeen@gmail.com>
2022-12-19 11:33:43 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function swap value
+ * @a: integer
+ * @b: integer
+ *Return: 0 success
+ */
+
+void swap_int (int *a, int *b)
+{
+int bridge;
+bridge = a;
+a = b;
+b = bridge;
+}
