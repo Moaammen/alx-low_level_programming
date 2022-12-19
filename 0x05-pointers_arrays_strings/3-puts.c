1dbcf5ba9b127b044ad79488f2b0c4cdb676b040
Moaammen <moaammmeen@gmail.com>
2022-12-19 12:30:25 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_puts - function print string
+ *@str: string
+ *Return: 0 success
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar (*str++);
+_putchar ('\n');
+}
+}
