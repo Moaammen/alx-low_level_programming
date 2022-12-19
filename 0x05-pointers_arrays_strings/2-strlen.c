db2177d4355f7eaec46e491d56862f2153b85d7c
Moaammen <moaammmeen@gmail.com>
2022-12-19 11:55:47 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+include "main.h"
+
+/**
+ * _strlen - function print string length
+ * @s: character
+ * Return: length Success
+ */
+
+int _strlen(char *s)
+{
+int counter = 0;
+
+while (*s != '\0')
+counter++;
+s++;
+}
+return (counter);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,13 @@ int _putchar(char c);
  * reset_to_98: function reset variable values to 98
  */
 void reset_to_98(int *n);
+
+/**
+ * swap_int: function swap values
+ */
+void swap_int(int *a, int *b);
+
+/**
+ * _strlen: function print length of string
+ */
+int _strlen(char *s);
