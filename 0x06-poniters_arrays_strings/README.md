56aa4e0d892da32e1ab7b28bb250693f78eb6c58
Moaammen <moaammmeen@gmail.com>
2022-12-22 11:44:36 -0800

    init commit

---
+++ 0x06-poniters_arrays_strings/README.md
@@ -0,0 +1,1 @@
+used pointers  to make strings function
---
+++ 0x06-poniters_arrays_strings/main.h
@@ -0,0 +1,17 @@
+/**
+ * _putchar: function print character
+ */
+
+int _putchar(char c);
+
+/**
+ * _strlen: function find lenght of string
+ */
+
+int _strlen(char *s);
+
+/**
+ * *_strcat: function concatenates two strings
+ */
+
+char *_strcat(char *dest, char *src);
