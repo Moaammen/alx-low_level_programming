7d53c106c1a8a67a03d1b59da326501195e99ae4
abdelmoamen <moaammmeen@gmail.com>
2022-12-23 20:00:40 +0200

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ while (s[i] != '\0') /* iterate through string */
 {
 
 /* if lowercase and prior char is separator, capitalize*/
-if ((s[i] >= 'a' && s[i] <= 'z')&&
+if ((s[i] >= 'a' && s[i] <= 'z') &&
 (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
 s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
 s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
