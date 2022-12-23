2399576094caefc38be2985fc8a2a25f2cae621e
Moaammen <moaammmeen@gmail.com>
2022-12-23 06:32:17 -0800

    all is upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,12 @@ char *string_toupper(char *str)
 {
 int i = 0;
 
-while (*(str + i) != '\0')
+while (s[i] != '\0')
 {
 
-if (*(str + i) >= 97 && *(str + i) <= 122)
+if (str[i] >= 97 && str[i] <= 122)
 {
-*(str + i ) = *(str + i) - 32;
+str[i] = str[i] - 32;
 }
 i++;
 }
