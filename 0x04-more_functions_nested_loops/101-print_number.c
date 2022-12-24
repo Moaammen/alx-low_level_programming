ad906a4a775bc63f0e1bbcd60cb8e717967ea35c
abdelmoamen <moaammmeen@gmail.com>
2022-12-24 17:18:08 +0200

    print numbers using putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,27 +9,57 @@
 
 void print_number(int n)
 {
-if (n <= 9 || n >= -9)
+if (n < 0)
 {
-_putchar(n + '0');
+_putchar('-');
+n = n * -1;
+if (n / 10 == 0)
+{
+_putchar(n % 10 + '0');
 }
-else if (((n > 9) && (n <= 99)) || ((n >= -99) && (n < -9)))
+else if (n / 100 == 0)
 {
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
 }
-else if (((n > 99) && (n <= 999)) || ((n >= -999) && (n < -99)))
+else if ((n / 1000 == 0) || (n / 1000 < 0))
 {
-_putchar((n / 100) + '0');
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
+_putchar(n / 100 + '0');
+_putchar((n / 100) / 10 + '0');
+_putchar(n % 10 + '0');
 }
-else if (((n > 999) && (n <= 9999)) || ((n >= -9999) && (n < -999)))
+else
 {
-_putchar((n / 1000) + '0');
-_putchar((n / 100) + '0');
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
+_putchar(n / 1000 + '0');
+_putchar((n / 1000) / 100 + '0');
+_putchar((n / 100) / 10 + '0');
+_putchar(n % 10 + '0');
+}
 }
 
+else
+{
+if (n / 10 == 0)
+{
+_putchar(n % 10 + '0');
+}
+else if (n / 100 == 0)
+{
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+}
+else if ((n / 1000 == 0) || (n / 1000 < 0))
+{
+_putchar(n / 100 + '0');
+_putchar((n / 100) / 10 + '0');
+_putchar(n % 10 + '0');
+}
+else
+{
+_putchar(n / 1000 + '0');
+_putchar((n / 1000) / 100 + '0');
+_putchar((n / 100) / 10 + '0');
+_putchar(n % 10 + '0');
+}
+}
 }
