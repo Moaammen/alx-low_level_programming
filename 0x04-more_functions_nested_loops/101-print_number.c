3149aa479b43898362dd2270492ea6b64844af02
abdelmoamen <moaammmeen@gmail.com>
2022-12-24 17:45:39 +0200

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,58 +9,22 @@
 
 void print_number(int n)
 {
+unsigned int n1;
+
 if (n < 0)
 {
-putchar('-');
-n = n * -1;
-if (n / 10 == 0)
-{
-putchar(n % 10 + '0');
-}
-else if (n / 100 == 0)
-{
-putchar(n / 10 + '0');
-putchar(n % 10 + '0');
-}
-else if ((n / 1000 == 0 )|| (n / 1000 < 0))
-{
-putchar(n / 100 + '0');
-putchar((n / 100) / 10 + '0');
-putchar(n % 10 + '0');
+n1 = -n;
+_putchar('-');
 }
 else
 {
-putchar(n / 1000 + '0');
-putchar((n / 1000) / 100 + '0');
-putchar((n / 100) / 10 + '0');
-putchar(n % 10 + '0');
-}
+n1 = n;
 }
 
-else
+if (n1 / 10)
 {
-if (n / 10 == 0)
-{
-putchar(n % 10 + '0');
-}
-else if (n / 100 == 0)
-{
-putchar(n / 10 + '0');
-putchar(n % 10 + '0');
-}
-else if ((n / 1000 == 0)|| (n / 1000 < 0))
-{
-putchar(n / 100 + '0');
-putchar((n / 100) / 10 + '0');
-putchar(n % 10 + '0');
-}
-else
-{
-putchar(n / 1000 + '0');
-putchar((n / 1000) / 100 + '0');
-putchar((n / 100) / 10 + '0');
-putchar(n % 10 + '0');
-}
-}
+print_number(n1 / 10);
 }
 
+_putchar((n1 % 10) + '0');
+}
