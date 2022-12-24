6fc4c1230a485e2e6364a66d8d7560af24fd8629
abdelmoamen <moaammmeen@gmail.com>
2022-12-24 17:40:20 +0200

    update

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+*print_number - function
+*Description: print integer using putchar
+*@n: integer
+*Return: 0 success
+*/
+
+
+void print_number(int n)
+{
+unsigned int tens, digit, positive = n;
+double t_beg = 1;
+
+if (n == 0)
+_putchar('0');
+else
+{
+if (n < 0)
+{
+positive = n * -1;
+_putchar('-');
+}
+
+while (t_beg <= positive)
+t_beg *= 10;
+tens = t_beg / 10;
+
+while (tens >= 1)
+{
+digit = positive / tens;
+_putchar(digit + '0');
+positive = (positive - (tens * digit));
+tens /= 10;
+}
+}
+}
+
+
