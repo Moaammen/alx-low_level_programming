cc3489c41b804e6a7d0c60f1a3098308562e728a
abdelmoamen <moaammmeen@gmail.com>
2022-12-24 16:36:08 +0200

    print int with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,22 +9,22 @@
 
 void print_number(int n)
 {
-if (n <= 9) || (n >= -9)
+if (n <= 9 || n >= -9)
 {
 _putchar(n + '0');
 }
-else if (n > 9 && n <= 99) || (n >= -99 && n < -9)
+else if (n > 9 && n <= 99 || n >= -99 && n < -9)
 {
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 }
-else if (n > 99 && n <= 999) || (n >= -999 && n < -99)
+else if (n > 99 && n <= 999 || n >= -999 && n < -99)
 {
 _putchar((n / 100) + '0');
 _putchar((n / 10) + '0');
 _putchar((n % 10) + '0');
 }
-else if (n > 999 && n <= 9999) || (n >= -9999 && n < -999)
+else if (n > 999 && n <= 9999 || n >= -9999 && n < -999)
 {
 _putchar((n / 1000) + '0');
 _putchar((n / 100) + '0');
