7e20477c0ea5b49788b9adb0f6b53594504a7d98
abdelmoamen <moaammmeen@gmail.com>
2022-12-31 19:33:51 +0200

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,17 +9,14 @@
 
 int _atoi(char *s)
 {
-
+int minus = 1;
+int value = 0;
 int i = 0;
 /** skipping any whitespaces , alphabet and symbols */
 while (s[i] == 32 || (s[i] >= 9 && s[i] <= 47) || (s[i] >= 58 && 127))
 {
 i++;
 }
-/** initialize minus sign and convert value*/
-
-int minus = 1;
-int value = 0;
 
 while (s[i] >= 48 && s[i] <= 57)
 {
