98d1fb50b0f4dad7d2d435cba831f8b27a737809
abdelmoamen <moaammmeen@gmail.com>
2023-01-02 23:16:56 +0200

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,14 +2,39 @@
 #include <stddef.h>
 
 /**
+*_strchr - Function.
+*Description: function that locates a character in a string.
+*@s: String.
+*@c: Charcter.
+*Return: Pointer pointer to the first occurrence of the character c.
+*/
+
+char *_strchr(char *s, char c)
+
+{
+while (*s != c && *s != '\0')
+{
+	s++;
+}
+
+if (*s == c)
+
+{
+	return (s);
+}
+else
+{
+	return (NULL);
+}
+}
+
+/**
 *_strspn - function.
 *Description: function that gets the length of a prefix substring.
 *@s: string.
 *@accept: Substring.
 *Return: unsigned int.
 */
-
-char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept)
 {
@@ -18,7 +43,7 @@ unsigned int size = 0;
 
 if ((s == NULL) || (accept == NULL))
 {
-return (0);
+return (size);
 }
 
 while (*s && _strchr(accept, *s++))
