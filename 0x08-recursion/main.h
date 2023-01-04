b108a32c997f5863ab363158bd19aa91ca26a52d
abdelmoamen <moaammmeen@gmail.com>
2023-01-04 21:14:33 +0200

    puts

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*_putchar - function print charcter.
+*@c: character.
+*Return: 0 success.
+*/
+
+int _putchar(char c);
+
+/**
+*_puts_recursion - Function.
+*Description: function that prints a string, followed by a new line.
+*@s: character
+*Return: 0 success.
+*/
+
+void _puts_recursion(char *s);
+
+#endif
