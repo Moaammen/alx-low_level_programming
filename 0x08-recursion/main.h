af5468d1e8c6c6d5abb63e988b71bc5fe39e856d
abdelmoamen <moaammmeen@gmail.com>
2023-01-04 21:31:30 +0200

    print reverse and str lenght

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -18,4 +18,20 @@ int _putchar(char c);
 
 void _puts_recursion(char *s);
 
+/**
+* _print_rev_recursion - print string in reverse
+* @s: string to print
+*/
+
+void _print_rev_recursion(char *s);
+
+/**
+* _strlen_recursion - return string length
+* @s: string
+* Return: string length
+*/
+
+int _strlen_recursion(char *s);
+
+
 #endif
