8a909e9d06a898371c04b0185a90fcd744d3169e
abdelmoamen <moaammmeen@gmail.com>
2023-01-07 12:42:34 +0200

    whats my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*Description: print program name using argv
+*@argc: integer for arg count.
+*@argv: array of string.
+*Return: 0 SUCCESS.
+*/
+
+
+int main(int argc, char *argv[])
+
+{
+
+printf("%s\n", argv[0]);
+
+return (0);
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+*ALX_LOW_LEVEL_PROGRAMMING.*<br>
+
+argc and argv<br>
