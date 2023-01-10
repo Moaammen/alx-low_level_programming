63f1fba2cc284982130a1d317d0c920ee30ace1f
abdelmoamen <moaammmeen@gmail.com>
2023-01-10 21:51:49 +0200

    checked malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 {
 
 char *ptr;
-int i = 0;
+unsigned int i = 0;
 if (size == 0)
 	return (NULL);
 
@@ -26,7 +26,7 @@ ptr = malloc(size * sizeof(char));
 if (ptr == NULL)
 	{
 
-	return(NULL);
+	return (NULL);
 
 	}
 
