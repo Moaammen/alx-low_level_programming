2f7fcea352553ce6a6c25c859e7afffa8c5824b8
abdelmoamen <moaammmeen@gmail.com>
2023-01-11 22:03:09 +0200

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -8,50 +8,38 @@
 *@height: integer.
 *Return: a pointer to a 2 dimensional array of integers.
 */
-
 int **alloc_grid(int width, int height)
-
 {
-
 int **ptr;
 int i, j;
-
 if ((width <= 0) || (height <= 0))
 {
 	return (NULL);
 }
 
-ptr = malloc(height * sizeof(int*));
-
+ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-
 for (i = 0; i < height; i++)
-	{
+{
 		ptr[i] = malloc(width * sizeof(int));
-
-		if (ptr == NULL)
-	{
+		if (ptr[i] == NULL)
+		{
 		for (i = 0; i < height; i++)
-				free (ptr[i]);
-
-		free (ptr);
-
+				{
+				free(ptr[i]);
+				}
+		free(ptr);
 		return (NULL);
 		}
-	}
 
-for (i = 0; i < height; i++)
-	{
 	for (j = 0; j < width; j++)
 		{
 			ptr[i][j] = 0;
 		}
-	}
-
+}
 return (ptr);
-
 }
 
