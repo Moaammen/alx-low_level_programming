0dde6721ee25f3dce28a530bf6e72a2fcff6849a
abdelmoamen <moaammmeen@gmail.com>
2023-01-11 21:41:18 +0200

    hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,11 +21,11 @@ if ((width <= 0) || (height <= 0))
 return (NULL);
 }
 
-ptr = malloc(width * sizeof(int*));
+ptr = malloc(height * sizeof(int*));
 
-for (i = 0; i < width; i++)
+for (i = 0; i < height; i++)
 {
-ptr[i] = malloc(height * sizeof(int));
+ptr[i] = malloc(width * sizeof(int));
 }
 
 if (ptr == NULL)
@@ -33,16 +33,16 @@ if (ptr == NULL)
 return (NULL);
 }
 
-for (i = 0; i < width; i++)
+for (i = 0; i < height; i++)
 {
-for (j = 0; j < height; j++)
+for (j = 0; j < width; j++)
 {
 ptr[i][j] = 0;
 }
 }
 return (ptr);
 
-for (i = 0; i < width; i++)
+for (i = 0; i < height; i++)
 	free (ptr[i]);
 
 free (ptr);
