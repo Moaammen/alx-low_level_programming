28c3e76ae06c9b855c83929297a507e4b9799694
abdelmoamen <moaammmeen@gmail.com>
2023-01-13 19:30:48 +0200

    fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,15 +12,43 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 
 {
-void *ptr;
+char *ptr;
+int *ptr1;
+unsigned int i;
 
 if ((nmemb <= 0) || (size <= 0))
 	return (NULL);
+
+
+if (size == 1)
+{
 
 ptr = malloc(nmemb * size);
 
 if (ptr == NULL)
 	return (NULL);
 
+for (i = 0; i < nmemb; i++)
+{
+
+ptr[i] = '0';
+
+}
 return (ptr);
+}
+else
+{
+ptr1 = malloc(nmemb * size);
+
+if (ptr1 == NULL)
+	return (NULL);
+
+for (i = 0; i < nmemb; i++)
+{
+
+ptr1[i] = 0;
+
+}
+return (ptr1);
+}
 }
