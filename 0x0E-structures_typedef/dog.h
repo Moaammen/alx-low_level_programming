78aa81ab5f98d3f9e0eda8f815a2ce9e5ac934f2
abdelmoamen <moaammmeen@gmail.com>
2023-01-16 23:05:00 +0200

    betty doc for my struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+*struct dog - struct for name, age, owner for a dog.
+*@name: pointer to string.
+*@age: float number for dog age.
+*@owner: pointer for string.
+*/
+
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 
