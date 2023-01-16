84d87190c469fae8e8eb231bae74f764f187f123
abdelmoamen <moaammmeen@gmail.com>
2023-01-16 22:08:48 +0200

    fix betty style and doc

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+
+/**
+*main - entry poitn.
+*Description: print file name using standard predefined macro.
+*Return: 0 success.
+*/
 int main(void)
 {
 
-printf ("%s\n", __FILE__);
+printf("%s\n", __FILE__);
 
 return (0);
 
