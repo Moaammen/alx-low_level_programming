d2413921e127b39899552a93df926ce7a7c090ae
abdelmoamen <moaammmeen@gmail.com>
2023-01-16 23:52:22 +0200

    fix betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,14 +22,3 @@ printf("Age: %f\n", d->age);
 : printf("Owner: %s\n", d->owner);
 }
 
-/**
-*void print_dog(struct dog *d)
-*{
-*if (d != NULL)
-*{
-*printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-*printf("Age: %f\n", (d->age) ? d->age : 0);
-*printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
-*}
-*}
-*/
