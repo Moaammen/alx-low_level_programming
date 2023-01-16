cd1114f4a9ca2be0a157405398b14214e577a743
abdelmoamen <moaammmeen@gmail.com>
2023-01-16 23:22:02 +0200

    include stdio

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include <dog.h>
-
+#include <stdio.h>
 /**
 *init_dog - function.
 *@d: pointer to struct.
@@ -9,6 +9,9 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+
+if (d == NULL)
+return;
 
 d->name = name;
 d->age = age;
