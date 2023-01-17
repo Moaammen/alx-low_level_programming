f7ede3d356bbd3b6fba20c0966f104a576d6693d
abdelmoamen <moaammmeen@gmail.com>
2023-01-17 21:19:34 +0200

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,36 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
-/**
- * len - find length of string
- * @str: string
- * Return: length
- */
-int len(char *str)
-{
-	int i;
-
-	for (i = 0; *(str + i); i++)
-	  ;
-	return (i);
-}
-/**
- * strcpy - copies the string pointed to by src,
- * including the terminating null byte (\0),
- * to the buffer pointed to by dest
- * @dest: copy source to this buffer
- * @src: this is the source to copy
- * Return: copy of original source
- */
-char *strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; i <= len(src); i++)
-		dest[i] = src[i];
-	return (dest);
-}
 /**
  * new_dog - create new instance of struct dog
  * @name: member
@@ -54,7 +26,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/* set values of struct members to copies of arguments or set to NULL */
 	if (name != NULL)
 	{
-		copy_of_name = malloc(len(name) + 1);
+		copy_of_name = malloc(strlen(name) + 1);
 		if (copy_of_name == NULL)
 		{
 			free(dog1);
@@ -67,7 +39,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (owner != NULL)
 	{
-		copy_of_owner = malloc(len(owner) + 1);
+		copy_of_owner = malloc(strlen(owner) + 1);
 		if (copy_of_owner == NULL)
 		{
 			free(copy_of_name);
