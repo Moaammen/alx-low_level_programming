18b6ebe87d9df368606d9f8be1fd52e4925e94f3
abdelmoamen <moaammmeen@gmail.com>
2023-01-20 00:50:27 +0200

    update get func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,30 +3,20 @@
 #include <string.h>
 
 /**
-<<<<<<< HEAD
  * get_op_func - ...
  * @s: ...
  *
  * Return: ...
  */
-=======
-  * get_op_func - ...
-  * @s: ...
-  *
-  * Return: ...
-  */
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 	{ "+", op_add },
 	{ "-", op_sub },
 	{ "*", op_mul },
-<<<<<<< HEAD
 	{ "/", op_div }
-=======
 	{ "/", op_div },
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
 	{ "%", op_mod },
 	{ NULL, NULL }
 	};
@@ -36,12 +26,7 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-<<<<<<< HEAD
-		
-=======
-
->>>>>>> 27b4117ba9e8f5489dde71fcc3fe32e3019c6dfa
-		i++;
+			i++;
 	}
 
 	return (0);
