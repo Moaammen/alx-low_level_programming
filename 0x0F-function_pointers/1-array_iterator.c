55e1d0e1f3a9281f6f842371df5864d8e38a2f86
abdelmoamen <moaammmeen@gmail.com>
2023-01-20 15:07:09 +0200

    Resolve the task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 size_t i;
 
-if ((array != NULL) && (action != NULL) && (size != NULL))
+if ((array != NULL) && (action != NULL) && (size != 0))
 {
 for (i = 0; i < size; i++)
 {
