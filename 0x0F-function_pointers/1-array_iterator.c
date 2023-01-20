8dc8f448b73e0e9beda01069813b165574c26a9d
abdelmoamen <moaammmeen@gmail.com>
2023-01-20 15:02:19 +0200

    Resolve the task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,26 @@
 #include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
 
 /**
- * array_iterator - ...
- * @array: ...
- * @size: ...
- * @action: ...
- *
- * Return: ...
- */
+*array_iterator -  function that executes a function given as a
+*parameter on each element of an array.
+*@array: array of integers.
+*@size: integer.
+*@action: pointer to function take ine parmeter.
+*Return: 0 success.
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
-{
-	unsigned int i = 0;
 
-	if (array != NULL && action != NULL && size > 0)
-	{
-		while (i < size)
-		{
-			action(array[i]);
-			i++;
-		}
-	}
+{
+
+int i;
+
+for (i = 0; i < size; i++)
+{
+
+action(array[i]);
+
+}
+
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
+#include <stddef.h>
 
 
 /**
@@ -9,5 +10,17 @@
 *Return: 0 Success.
 */
 void print_name(char *name, void (*f)(char *));
+
+/**
+*array_iterator -  function that executes a function given as a
+*parameter on each element of an array.
+*@array: array of integers.
+*@size: integer.
+*@action: pointer to function take ine parmeter.
+*Return: 0 success.
+*/
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+
 
 #endif
