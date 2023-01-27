cf5d1ec8259dc1d11a736ca0d99ee16b4713d3f9
abdelmoamen <moaammmeen@gmail.com>
2023-01-27 18:19:55 +0200

    add if else cond

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
 
 /**
 *print_list - function print all the elements of list_t.
@@ -13,19 +12,20 @@ size_t print_list(const list_t *h)
 {
 size_t nodes;
 nodes = 0;
-if (h == NULL)
-{
-printf ("[0] (nil)\n");
-exit(1);
-}
+
 
 while (h != NULL)
 {
-printf("[%d]%s\n",h->len, h->str);
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
 h = h->next;
 nodes++;
 }
-
 return (nodes);
-
 }
