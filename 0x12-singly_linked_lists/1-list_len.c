477b1651ecb29cf73331ae314ff59a2e972f19a2
abdelmoamen <moaammmeen@gmail.com>
2023-01-27 18:32:29 +0200

    lists len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len -  function that returns the number of elements
+ * in a linked list_t list.
+ * @h: linked list
+ * Return: number of elements.
+*/
+size_t list_len(const list_t *h)
+{
+
+size_t len;
+len = 0;
+
+while (h != NULL)
+{
+len++;
+}
+
+return (len);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,9 +21,17 @@ struct list_s *next;
 
 /**
 *print_list - function print all the elements of list_t.
-*@h: element of list.
+*@h: linked list.
 *Return: the number of nodes.
 */
 
 size_t print_list(const list_t *h);
+
+/**
+ * list_len -  function that returns the number of elements
+ * in a linked list_t list.
+ * @h: linked list
+ * Return: number of elements.
+*/
+size_t list_len(const list_t *h);
 #endif
