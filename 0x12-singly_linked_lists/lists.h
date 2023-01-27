df3388c2313e7f4c45b86088b9e25339ca4d3676
abdelmoamen <moaammmeen@gmail.com>
2023-01-27 20:58:02 +0200

    add node end

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -44,4 +44,12 @@ size_t list_len(const list_t *h);
 */
 list_t *add_node(list_t **head, const char *str);
 
+/**
+ * add_node_end - function that adds anew node at the end.
+ * @head: pointer to pointer to a list.
+ * @str: string in the list.
+ * Return: pointer to list.
+*/
+list_t *add_node_end(list_t **head, const char *str);
+
 #endif
